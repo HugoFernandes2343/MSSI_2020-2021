ca29c100ee34954015c40c032a0a7813b990a952
Hugo Fernandes <hugo.d.fernandes@hotmail.com>
2021-04-12 17:55:30 +0100

    tutorial 2 p1

---
+++ src/histogram_module.py
@@ -0,0 +1,15 @@
+from mesa.visualization.ModularVisualization import VisualizationElement
+
+class HistogramModule(VisualizationElement):
+    package_includes = ["Chart.min.js"]
+    local_includes = ["HistogramModule.js"]
+
+    def __init__(self, bins, canvas_height, canvas_width):
+        self.canvas_height = canvas_height
+        self.canvas_width = canvas_width
+        self.bins = bins
+        new_element = "new HistogramModule({}, {}, {})"
+        new_element = new_element.format(bins,
+                                         canvas_width,
+                                         canvas_height)
+        self.js_code = "elements.push(" + new_element + ");"
