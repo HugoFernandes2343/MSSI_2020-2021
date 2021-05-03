457ba8ae1d3c650d96ea987779e1c54e462a9cfb
Gonçalo Pereira <goncalo.valpneus@hotmail.com>
2021-05-03 04:10:18 +0100

    cars only spawns in road

--- src/parking_lot_model.py
+++ src/parking_lot_model.py
@@ -40,65 +40,66 @@ class MoneyModel(Model):
         self.grid = MultiGrid(width, height, True)
         self.schedule = RandomActivation(self)
         self.running = True
-
-        # TODO turn this into proper car agents that will only be able to walk on black spots
-        for i in range(self.num_agents):
-            a = MoneyAgent(i, self)
-            self.schedule.add(a)
-            # Add the agent to a random grid cell
-            x = self.random.randrange(self.grid.width)
-            y = self.random.randrange(self.grid.height)
-            self.grid.place_agent(a, (x, y))
-
+        roads =[]
+        
         #Creating the roads
         i = 1
         while i <= 17:
             self.num_agents = self.num_agents + 1
             a = Tile(self.num_agents, self)
             self.grid.place_agent(a, (i, 1))
+            roads.append((i,1))
             i = i + 1
         i = 1
         while i <= 17:
             self.num_agents = self.num_agents + 1
             a = Tile(self.num_agents, self)
             self.grid.place_agent(a,(18,i))
+            roads.append((18,i))
             i = i + 1
         i = 18
         while i > 1 :
             self.num_agents = self.num_agents + 1
             a = Tile(self.num_agents, self)
             self.grid.place_agent(a,(i,18))
+            roads.append((i,18))
             i = i - 1
         i = 18
         while i > 1 :
             self.num_agents = self.num_agents + 1
             a = Tile(self.num_agents, self)
             self.grid.place_agent(a,(1,i))
+            roads.append((1,i))
             i = i - 1
         i = 2
         while i <= 17 :
             self.num_agents = self.num_agents + 1
             a = Tile(self.num_agents, self)
             self.grid.place_agent(a,(i,14))
+            roads.append((i,14))
             i = i + 1
 
         #Creating the exit
         self.num_agents = self.num_agents + 1
         a = Tile(self.num_agents, self)
         self.grid.place_agent(a,(7,2))
+        roads.append((7,2))
 
         self.num_agents = self.num_agents + 1
         a = Tile(self.num_agents, self)
         self.grid.place_agent(a,(7,3))
+        roads.append((7,3))
 
         #Creating the entrance todo add a function to the movement that makes cars that enter go to a parking tile
         self.num_agents = self.num_agents + 1
         a = Tile(self.num_agents, self)
         self.grid.place_agent(a,(12,2))
+        roads.append((12,2))
 
         self.num_agents = self.num_agents + 1
         a = Tile(self.num_agents, self)
         self.grid.place_agent(a,(12,3))
+        roads.append((12,3))
 
         ## The three blocks that will hold the cars
         # todo make it so the number of blocks (1-3) depends on the amount of parking tiers
@@ -106,14 +107,17 @@ class MoneyModel(Model):
         self.num_agents = self.num_agents + 1
         a = Tile(self.num_agents, self)
         self.grid.place_agent(a,(7,8))
+        roads.append((7,8))
 
         self.num_agents = self.num_agents + 1
         a = Tile(self.num_agents, self)
         self.grid.place_agent(a,(9,8))
+        roads.append((9,8))
 
         self.num_agents = self.num_agents + 1
         a = Tile(self.num_agents, self)
         self.grid.place_agent(a,(11,8))
+        roads.append((11,8))
 
         #Creating the parking lot outline
         i = 4
@@ -121,6 +125,7 @@ class MoneyModel(Model):
             self.num_agents = self.num_agents + 1
             a = Tile(self.num_agents, self)
             self.grid.place_agent(a,(4,i))
+            roads.append((4,i))
             i = i + 1
 
         i = 4
@@ -128,6 +133,7 @@ class MoneyModel(Model):
             self.num_agents = self.num_agents + 1
             a = Tile(self.num_agents, self)
             self.grid.place_agent(a,(15,i))
+            roads.append((15,i))
             i = i + 1
 
         i=4
@@ -135,29 +141,35 @@ class MoneyModel(Model):
             self.num_agents = self.num_agents + 1
             a = Tile(self.num_agents,self)
             self.grid.place_agent(a,(i,12))
+            roads.append((i,12))
             i = i + 1
 
         self.num_agents = self.num_agents + 1
         a = Tile(self.num_agents, self)
         self.grid.place_agent(a,(5,4))
+        roads.append((5,4))
 
         self.num_agents = self.num_agents + 1
         a = Tile(self.num_agents, self)
         self.grid.place_agent(a,(6,4))
+        roads.append((6,4))
 
         self.num_agents = self.num_agents + 1
         a = Tile(self.num_agents, self)
         self.grid.place_agent(a,(14,4))
+        roads.append((14,4))
 
         self.num_agents = self.num_agents + 1
         a = Tile(self.num_agents, self)
         self.grid.place_agent(a,(13,4))
+        roads.append((13,4))
 
         i = 6
         while i < 9 :
             self.num_agents = self.num_agents + 1
             a = Tile(self.num_agents,self)
             self.grid.place_agent(a,(i,5))
+            roads.append((i,5))
             i = i + 1
 
         i = 13
@@ -165,6 +177,7 @@ class MoneyModel(Model):
             self.num_agents = self.num_agents + 1
             a = Tile(self.num_agents,self)
             self.grid.place_agent(a,(i,5))
+            roads.append((i,5))
             i = i - 1
 
         i = 8
@@ -172,7 +185,24 @@ class MoneyModel(Model):
             self.num_agents = self.num_agents + 1
             a = Tile(self.num_agents,self)
             self.grid.place_agent(a,(i,4))
+            roads.append((i,4))
             i = i + 1
+
+        # TODO turn this into proper car agents that will only be able to walk on black spots
+        for i in range(self.num_agents):
+            a = MoneyAgent(i, self)
+            self.schedule.add(a)
+            done = 0
+            while not done:
+                # Add the agent to a random grid cell
+                x = self.random.randrange(self.grid.width)
+                y = self.random.randrange(self.grid.height)
+                if (x,y) in roads:
+                    done = 1
+                    self.grid.place_agent(a, (x, y))
+
+
+
 
     # self.datacollector = DataCollector(
         #  model_reporters={"Gini": compute_gini},
